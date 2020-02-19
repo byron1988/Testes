6a7cc7c07c2ea64434dfe5a56d1fb7446c1e4564
ubirani <you@example.com>
2020-02-18 23:31:37 -0300

    melhorando os testes do converte numeros romanos

--- source/numeros_romanos.py
+++ source/numeros_romanos.py
@@ -1,12 +1,12 @@
 class ConverteNumeroRomano:
-
-    def __init__(self, numero_romano):
-        self.numero_romano = numero_romano
+    
+    def __init__(self):
         self.digito_map = {"M":1000, "D":500, "C":100, "L":50, "X":10, "V":5, "I":1}
         
 
-    def converte_para_decimal(self):
+    def converte_para_decimal(self, numero_romano):
         val = 0
-        for char in self.numero_romano:
+        for char in numero_romano:
             val += self.digito_map[char]
         return val
+        
--- source/tests_numeros_romanos.py
+++ source/tests_numeros_romanos.py
@@ -1,29 +1,30 @@
 import unittest
+import sys
 from numeros_romanos import ConverteNumeroRomano
 
 class TestConverteNumeroRomano(unittest.TestCase):
     
-    def test_mil(self):
-        value = ConverteNumeroRomano('M')
-        self.assertEqual(1000, value.converte_para_decimal())
+    def setUp(self):
+        print("Contruir o objeto da class ConverteNumeroRomano")
+        self.cnr = ConverteNumeroRomano()
 
-    def test_cem(self):
-        value = ConverteNumeroRomano('C')
-        self.assertEqual(100, value.converte_para_decimal())     
+    def tearDown(self):
+        print("Destruir o objeto da class ConverteNumeroRomano")
+        self.cnr = None
+    
+    @unittest.skipIf( sys.version_info > (2,7), "Não suporta versões superiores a 2.7")
+    def test_mil(self):
+        self.assertEqual(1000, self.cnr.converte_para_decimal('M'))
+
+    def test_cem(self):        
+        self.assertEqual(100, self.cnr.converte_para_decimal('C'))     
 
     def test_ciquenta(self):
-        value = ConverteNumeroRomano('L')
-        self.assertEqual(50, value.converte_para_decimal())
+        self.assertEqual(50, self.cnr.converte_para_decimal('L'))
 
-    def test_dez(self):
-        value = ConverteNumeroRomano('X')
-        self.assertEqual(10, value.converte_para_decimal())
-
-    def test_vazio(self):
-        value = ConverteNumeroRomano('')
-        self.assertTrue(value.converte_para_decimal() == 0)
-        self.assertFalse(value.converte_para_decimal() > 0)
-
+    def test_vazio(self):        
+        self.assertTrue(self.cnr.converte_para_decimal('') == 0)
+        self.assertFalse(self.cnr.converte_para_decimal('') > 0)
 
 if __name__ == '__main__':
     unittest.main()
